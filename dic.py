6a1f7d31689ef68eb3b1b5419aa1684882d30340
BitCoin4Life <kajan.kandasamy@elev.it-gymnasiet.se>
2018-02-16 14:48:27 +0100

    hej
    hi

---
+++ dic.py
@@ -0,0 +1,9 @@
+lexikon1={"One":"Uno","Two":"Dos","Three":"tres","Four":"Cuatro","Five":"Cinco"}
+lexikon2={"Six":"Seis","Seven":"Siete","Eight":"Ocho","Nine":"Nueve","Ten":"Diez"}
+
+lexikon3={}
+
+lexikon3.update(lexikon2)
+lexikon3.update(lexikon1)
+
+print lexikon3
